ab7f6d5481e1964a7470a49b06fed8827dd568df
Paul Littlefair <vodka2u@gmail.com>
2012-06-01 10:03:49 +0100

    Change the way the ID is applied

--- jquery.lightArea/jquery.lightArea.js
+++ jquery.lightArea/jquery.lightArea.js
@@ -17,8 +17,7 @@ options : either an options object or true if you want to remove the lightarea
 		ops = $.extend(true, {},$.fn.lightArea.defaults, ops);
 		//CHECK IF BROWSER IS IE AND LESS THAN 9, IF IT IS CHANGE OPACITY UNITS TO TENS
 		var opacity = $.browser.msie && +$.browser.version <=8 ? ops.opacity * 100 : ops.opacity,
-		
-		//MAKE SURE spanCSS HAS POSITION SET
+			//MAKE SURE spanCSS HAS POSITION SET
 			spanCSS = $.extend({}, ops.spanCSS, {
 				display:"none",
 				position:"absolute"
@@ -31,13 +30,12 @@ options : either an options object or true if you want to remove the lightarea
 				filter:"alpha(opacity=" + opacity + ")",
 				position:"absolute"
 			}),
-		
 			//CREATE A DIV AND GIVE IT STYLE/CLASS
 			div = $("<div />", {
 				css : divCSS,
 				"class" : ops.divClass
 			}),
-			//CREATE SPAN
+			//CREATE SPAN IF ops.spanTexT IS TRUE OTHERWISE USE EMPTY STRING
 			span = $("<span />", {
 				css : spanCSS,
 				"class" : ops.spanClass,
@@ -69,17 +67,18 @@ options : either an options object or true if you want to remove the lightarea
 			//IF ADDING THE LIGHTAREA
 			if(options !== true){
 				var divClone = div.clone(),
-					spanClone = "",
-								
+					spanClone = "",				
 					//GET THE POSITION OF THE ELEMENT TO COVER RELATIVE TO DOCUMENT
 					thisOffset = $this.offset();
 				
-				//CREATE A RANDOM ID FOR THE LIGHTAREA AND ATTACH TO DATA OBJECT
-				id = +new Date() + Math.floor(Math.random()*1e6);
-				
 				//GIVE THE ELEMENT THAT IS BEING COVERED AN ID IF IT HASN'T GOT ONE
 				if(!eleID){
+					//CREATE A RANDOM ID FOR THE LIGHTAREA AND ATTACH TO DATA OBJECT
+					id = +new Date() + Math.floor(Math.random()*1e6);
 					eleID = this.id = "lightArea-" + id;
+				//ELSE USE THE EXISTING ID OF THE ELEMENT BEING COVERED
+				}else{
+					id = (eleID).split("lightArea-")[1] || eleID;
 				}
 				
 				divClone.attr("id", "lightAreaDiv-" + id);
@@ -120,11 +119,11 @@ options : either an options object or true if you want to remove the lightarea
 		backgroundColor : "#000",
 		fadeIn : "fast",
 		opacity : "0.3",
-		spanShow : false,
-		spanText : "",
-		spanClass : "lightArea",
+		spanShow : true,
+		spanText : "LOADING...",
+		spanClass : "ost-loading",
 		spanCSS : {padding:"5px",border:"1px solid #000",background:"#fff",fontWeight:"bold",fontSize:"14px",color:"#000","z-index":1001},
-		divClass : "lightArea",
+		divClass : "ost-loading",
 		divCSS : {"z-index":1000}
 	};
 }(window.jQuery));
