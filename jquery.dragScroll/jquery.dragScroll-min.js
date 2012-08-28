4f5be032ebd14e422c3e0d27ddb4b0a052f4b047
Paul Littlefair <vodka2u@gmail.com>
2012-08-28 08:50:21 +0100

    jquery.dragScroll plugin added.

---
+++ jquery.dragScroll/jquery.dragScroll-min.js
@@ -0,0 +1,17 @@
+/*
+* dragScroll- a jQuery plugin for creating a dragging scroll action on an element with overflow hidden
+* version - 0.02
+* Copyright (c) 2012 Paul Littlefair
+* -------------------------------------------------------
+* Dual licensed under the MIT and GPL licenses.
+* - http://www.opensource.org/licenses/mit-license.php
+* - http://www.gnu.org/copyleft/gpl.html
+* -------------------------------------------------------
+* -------------------------------------------------------
+OPTIONS:
+scrollWheel : Boolean. IF THE SCROLL WHEELS SHOULD SCROLL THE CONTAINER
+scrollWheelJump : Number. NUMBER OF PIXELS TO MOVE THE CONTAINER UP OR DOWN WHEN MOUSE WHEEL IS USED
+*/
+(function(d){d.fn.dragScroll=function(l){function m(b){var c=parseInt(b.css("left"),10),d=parseInt(b.css("top"),10),g=c+b.outerWidth(),b=d+b.outerHeight();return{left:c,top:d,right:g,bottom:b}}function i(b,c){b.style.cursor=c?"n-resize":"default";return c}var j=d.extend({},d.fn.dragScroll.defaults,l);return this.each(function(){var b,c,h,g=+new Date+Math.floor(1E6*Math.random()),e=this,k=d(this),f=m(k);k.wrapInner('<div id="dragScrollInner-'+g+'" />').css({"-moz-user-select":"-moz-none","-khtml-user-select":"none",
+"-webkit-user-select":"none","-ms-user-select":"none","user-select":"none"});h=d("#dragScrollInner-"+g).bind("mousedown",function(a){b=a.clientY+e.scrollTop-f.top;c=i(this,!0)}).bind("mouseup",function(){c=i(this)}).bind("mousemove",function(a){c&&(a=a.clientY+e.scrollTop-f.top,e.scrollTop=a<b?e.scrollTop+(b-a):e.scrollTop-(a-b))});j.scrollWheel&&h.bind("mousewheel",function(a){e.scrollTop=0<a.wheelDelta/120?e.scrollTop-j.scrollWheelJump:e.scrollTop+j.scrollWheelJump});d(document).bind("mousemove",
+function(a){if(c&&(a.clientX>f.right||a.clientX<f.left||a.clientY<f.top||a.clientY>f.bottom))c=i(h.get(0))});this.onselectstart=function(){return!1}})};d.fn.dragScroll.defaults={scrollWheel:!1,scrollWheelJump:30}})(jQuery);
---
+++ jquery.dragScroll/jquery.dragScroll.js
@@ -0,0 +1,129 @@
+/*
+* dragScroll- a jQuery plugin for creating a dragging scroll action on an element with overflow hidden
+* version - 0.02
+* Copyright (c) 2012 Paul Littlefair
+* -------------------------------------------------------
+* Dual licensed under the MIT and GPL licenses.
+* - http://www.opensource.org/licenses/mit-license.php
+* - http://www.gnu.org/copyleft/gpl.html
+* -------------------------------------------------------
+* -------------------------------------------------------
+OPTIONS:
+scrollWheel : Boolean. IF THE SCROLL WHEELS SHOULD SCROLL THE CONTAINER
+scrollWheelJump : Number. NUMBER OF PIXELS TO MOVE THE CONTAINER UP OR DOWN WHEN MOUSE WHEEL IS USED
+*/
+(function($){
+	/*
+	* @description CREATES A DRAGGABLE SCROLLING DIV (overflow:auto)
+	*/
+	$.fn.dragScroll = function(opts){
+		//EXTEND THE DEFAULT OPTIONS
+		var options = $.extend({},$.fn.dragScroll.defaults, opts);
+		/*
+		* @description THE POSITION OF THE MOUSE RELATIVE TO CONTENT OF THE DIV
+		* @param {Object<Event>} e
+		* @param {Object<jQuery} elem
+		* @param {Number} topPos
+		* @returns {Number}
+		*/
+		function getMousePosition(e, elem, topPos){
+			return e.clientY + elem.scrollTop - topPos;
+		}
+		/*
+		* @description GETS THE COORDIATES OF THE ELEMENT
+		* @param {Object<jQuery>} $elem
+		* @returns {Object}
+		*/
+		function getCoords($elem){
+			var leftPos = parseInt($elem.css("left"),10),
+			topPos = parseInt($elem.css("top"),10),
+			rightPos = leftPos + $elem.outerWidth(),
+			bottomPos = topPos + $elem.outerHeight();
+
+			return{
+				left : leftPos,
+				top : topPos,
+				right : rightPos,
+				bottom : bottomPos
+			};
+		}
+		/*
+		* @description ACTIONS THAT OCCUR WHEN STATE (mousedown/mouseup) CHANGES
+		* @param {Object<Element>} elem
+		* @param {Boolean} active
+		* @returns {Boolean|Undefined}
+		*/
+		function changeState(elem, active){
+			elem.style.cursor = active ? "n-resize" : "default";
+			return active;
+		}
+
+		return this.each(function(){
+			var startY, mouseDown, $holder,
+				//CREATE A UNIQUE ID
+				uid = +new Date() + Math.floor(Math.random()*1e6),
+				drag_container = this,
+				$drag_container = $(this),
+				coords = getCoords($drag_container);
+
+			//WRAP THE ELEMENT IN A DIV SO USING THE SCROLL BAR DOESN'T TRIGGER MOUSEDOWN/MOUSEUP
+			$drag_container.wrapInner('<div id="dragScrollInner-' + uid + '" />')
+			//STOP TEXT BEING SELECTED
+				.css({"-moz-user-select":"-moz-none","-khtml-user-select":"none","-webkit-user-select":"none","-ms-user-select":"none","user-select":"none"});
+
+			//ADD EVENT HANDLERS TO INNER DIV
+			$holder = $("#dragScrollInner-" + uid)
+				.bind("mousedown", function(e){
+					//GET THE ORIGINAL POSITION OF THE MOUSE WHEN DOWN
+					startY = getMousePosition(e, drag_container, coords.top);
+					mouseDown = changeState(this, true);
+				})
+				.bind("mouseup", function(){
+					mouseDown = changeState(this);
+				})
+				.bind("mousemove", function(e){
+					if(mouseDown){
+						//GET THE CURRENT POSITION OF THE MOUSE
+						var currentY = getMousePosition(e, drag_container, coords.top);
+						//IF THE CURRENT MOUSE POS IS LESS THAN START POS (MOUSE MOVING UP/PAGE DOWN) ADD THE DIFFERENCE TO THE SCROlLTOP
+						if(currentY < startY){
+							drag_container.scrollTop = drag_container.scrollTop + (startY - currentY);
+							//IF THE CURRENT MOUSE POS IS GREATER THAM START POS (MOUSE MOVING DOWN/PAGE UP) MINUS THE DIFFERENCE FROM THE SCROlLTOP
+						}else{
+							drag_container.scrollTop = drag_container.scrollTop - (currentY - startY);
+						}
+					}
+				});
+			//IF SCROLL WHEEL ACTION IS REQUIRED
+			if(options.scrollWheel){
+				$holder.bind("mousewheel", function(e){
+					//IF THE MOUSE WHEEL IS GOING UP MINUS FROM SCROLLTOP
+					if(e.wheelDelta / 120 > 0){
+						drag_container.scrollTop = drag_container.scrollTop - options.scrollWheelJump;
+					//IF THE MOUSE WHEEL IS GOING DOWN ADD TO SCROLLTOP
+					}else{
+						drag_container.scrollTop = drag_container.scrollTop + options.scrollWheelJump;
+					}
+				});
+			}
+
+
+			//CHANGE STATE IF MOUSE MOVES OUTSIDE OF ELEMENT
+			$(document).bind("mousemove", function(e){
+				if(mouseDown){
+					//IF THE MOUSE MOVES OUT OF THE DIV STOP SCROLLING
+					if(e.clientX > coords.right || e.clientX < coords.left || e.clientY < coords.top || e.clientY >coords.bottom){
+						mouseDown = changeState($holder.get(0));
+					}
+				}
+			});
+			//STOP THE ELEMENTS TEXT BEING SELECTED
+			this.onselectstart = function(){return false;};
+		});
+	};
+	
+	$.fn.dragScroll.defaults = {
+		scrollWheel : false,
+		scrollWheelJump : 30
+	};
+}(jQuery));
